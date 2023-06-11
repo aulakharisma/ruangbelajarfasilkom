6f85df86c45fe2254c7073d91100a7bc306203ee
aulakharisma <aulakharismah21@gmail.com>
2023-06-11 13:51:05 +0700

    update database

--- ruangbelajar.sql
+++ ruangbelajar.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jun 06, 2023 at 05:21 PM
+-- Generation Time: Jun 11, 2023 at 08:50 AM
 -- Server version: 10.4.27-MariaDB
 -- PHP Version: 8.1.12
 
@@ -33,6 +33,13 @@ CREATE TABLE `admin` (
   `password` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
+--
+-- Dumping data for table `admin`
+--
+
+INSERT INTO `admin` (`id`, `username`, `password`) VALUES
+(1, 'admin1', 'password1');
+
 -- --------------------------------------------------------
 
 --
@@ -41,16 +48,25 @@ CREATE TABLE `admin` (
 
 CREATE TABLE `materi` (
   `id` int(11) NOT NULL,
-  `id_admin` int(11) NOT NULL,
-  `id_user` int(11) NOT NULL,
+  `id_admin` int(11) DEFAULT NULL,
+  `id_user` int(11) DEFAULT NULL,
   `judul` varchar(255) NOT NULL,
   `id_matkul` int(11) NOT NULL,
   `dok_materi` varchar(255) NOT NULL,
+  `Kategori` enum('Tugas','Materi dosen','Lain-Lain') NOT NULL,
   `deskripsi` text NOT NULL,
   `cover` varchar(100) NOT NULL,
   `CREATED_AT` datetime NOT NULL,
   `UPDATED_AT` datetime NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `materi`
+--
+
+INSERT INTO `materi` (`id`, `id_admin`, `id_user`, `judul`, `id_matkul`, `dok_materi`, `Kategori`, `deskripsi`, `cover`, `CREATED_AT`, `UPDATED_AT`) VALUES
+(3, NULL, 2, 'Prototyping aplikasi BookingYuk', 4, 'materi1.pdf', 'Tugas', 'tugas1', 'covermateri1', '2023-06-11 08:43:28', '2023-06-11 08:43:28'),
+(4, NULL, 2, 'Sorting data menggunakan metode xxx', 2, 'materi2.pdf', 'Materi dosen', 'materi dari dosen x untuk mata kuliah struktur data', 'covermateri2.png', '2023-06-11 08:43:28', '2023-06-11 08:43:28');
 
 -- --------------------------------------------------------
 
@@ -67,6 +83,16 @@ CREATE TABLE `matkul` (
   `UPDATED_AT` datetime NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
+--
+-- Dumping data for table `matkul`
+--
+
+INSERT INTO `matkul` (`id`, `nama_matkul`, `deskripsi`, `prodi`, `CREATED_AT`, `UPDATED_AT`) VALUES
+(1, 'Metode Numerik', 'mata kuliah ini dimaksudkan untuk memberi kemampuan pada mahasiswa tentang topik-topik dasar dari metode numerik yaitu galat, interpolasi, penghampiran fungsi, sistem persamaan linier, integrasi numerik, dan persamaan diferensial biasa sehingga diharapkan mahasiswa memperoleh pemahaman intuitif tentang beberpa ametode numerik yang digunakan untuk menyelesaikan masalah-masalah dasar dalam metode numerik, juga mendapatkan apresiasi mengenai konsep galat dan kebutuhan untuk menganalisis dan mekanismenya, serta dapat mengembangkan pengalaman dalam menerapkan metode numerik dengan menggunakan komputer.', 'informatika', '2023-06-11 08:26:46', '2023-06-11 08:26:46'),
+(2, 'Struktur Data', 'Matakuliah ini sebagian besar teori yang diikuti dengan praktikum. Melalui Matakuliah ini mahasiswa mampu untuk memahami konsep, teknik dan manipulasi pengorganisasian sebuah data yang diimplementasikan kedalam sebuah bahasa pemrograman seperti array, string, matriks, record. Selain itu matakuliah ini juga memberi pemahaman tentang list linear, multi link list, non linier link list, jenis-jenis pengurutan, pencarian, graf, dan tree.', 'informatika', '2023-06-11 08:26:46', '2023-06-11 08:26:46'),
+(3, 'Pemrograman Framework', 'Mata kuliah ini merupakan lanjutan dari pemrograman website, di mana mahasiswa akan mempelajari tentang beberapa framework, seperti codeigniter, react, node, dll.', 'informatika', '2023-06-11 08:26:46', '2023-06-11 08:26:46'),
+(4, 'UI UX', 'Mata kuliah ini mempelajari tentang hubungan manusia dengan komputer', 'umum', '2023-06-11 08:26:46', '2023-06-11 08:26:46');
+
 -- --------------------------------------------------------
 
 --
@@ -78,6 +104,16 @@ CREATE TABLE `users` (
   `username` varchar(255) NOT NULL,
   `password` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `username`, `password`) VALUES
+(1, '20081010192', 'passowrd192'),
+(2, '20081010193', 'password193'),
+(3, '20082010102', 'password102'),
+(4, '20083010090', 'password090');
 
 -- --------------------------------------------------------
 
@@ -93,6 +129,15 @@ CREATE TABLE `user_profile` (
   `CREATED_AT` datetime NOT NULL,
   `UPDATED_AT` datetime NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `user_profile`
+--
+
+INSERT INTO `user_profile` (`id`, `id_user`, `nama`, `prodi`, `CREATED_AT`, `UPDATED_AT`) VALUES
+(1, 1, 'Aula Kharismah', 'informatika', '2023-06-11 08:23:49', '2023-06-11 08:23:49'),
+(2, 2, 'Frisda Dita Isnaini', 'informatika', '2023-06-11 08:24:22', '2023-06-11 08:24:22'),
+(3, 3, 'Daud Arya Rafa', 'sisteminformasi', '2023-06-11 08:26:17', '2023-06-11 08:26:17');
 
 --
 -- Indexes for dumped tables
@@ -140,31 +185,31 @@ ALTER TABLE `user_profile`
 -- AUTO_INCREMENT for table `admin`
 --
 ALTER TABLE `admin`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT for table `materi`
 --
 ALTER TABLE `materi`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT for table `matkul`
 --
 ALTER TABLE `matkul`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT for table `user_profile`
 --
 ALTER TABLE `user_profile`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- Constraints for dumped tables
